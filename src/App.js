857ba7268ccbd38d5cc72650fcab3496796a7b19
Chinni <enjarappachinni2001@gmail.com>
2022-08-02 17:41:52 +0530

    home1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import './App.css';
 
 import Homepage from "./components/Homepage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Homepage />
     </div>
   );
