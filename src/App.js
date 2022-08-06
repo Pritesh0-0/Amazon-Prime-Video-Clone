d41483b322e2c9d10a102ea1ab3e165f6fafc079
Pritesh0-0 <100704776+Pritesh0-0@users.noreply.github.com>
2022-08-06 18:16:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Paymentspage from './components/Paymentspage';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Paymentspage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function header() {
+    return (
+        <div>
+            <meta charSet="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css' />
+            <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"></link>
+        </div>
+    )
+}
+
+export default header;
